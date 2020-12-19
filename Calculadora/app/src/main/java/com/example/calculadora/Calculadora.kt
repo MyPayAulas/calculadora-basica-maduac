ad63ff0211c83f20dff64623362745061a5f8cbd
Maria Eduarda Assunção Cavalcante <meac.eng19@uea.edu.br>
2020-12-19 18:40:52 -0400

    feat: Listener dos botões

--- Calculadora/app/src/main/java/com/example/calculadora/Calculadora.kt
+++ Calculadora/app/src/main/java/com/example/calculadora/Calculadora.kt
@@ -10,5 +10,32 @@ class Calculadora : AppCompatActivity() {
         setContentView(R.layout.calculadora)
 
         supportActionBar!!.hide() //Esconder toolbar
+
+        zero.setOnClickListener { CalculoIncial("0", true ) }
+        um.setOnClickListener { CalculoIncial("1", true ) }
+        dois.setOnClickListener { CalculoIncial("2", true ) }
+        tres.setOnClickListener { CalculoIncial("3", true ) }
+        quatro.setOnClickListener { CalculoIncial("4", true ) }
+        cinco.setOnClickListener { CalculoIncial("5", true ) }
+        seis.setOnClickListener { CalculoIncial("6", true ) }
+        sete.setOnClickListener { CalculoIncial("7", true ) }
+        oito.setOnClickListener { CalculoIncial("8", true ) }
+        nove.setOnClickListener { CalculoIncial("9", true ) }
+    }
+
+    //começar o calculo e obtenção de dados
+    fun CalculoIncial(string:String, limpar_dados: Boolean){
+        if (resultado.text.isNotEmpty()){
+            formula.text = ""
+        }
+
+        if(limpar_dados){
+            resultado.text = ""
+            formula.append(string)
+        }else{
+            formula.append(resultado.text)
+            formula.append(string)
+            resultado.text = ""
+        }
     }
 }
