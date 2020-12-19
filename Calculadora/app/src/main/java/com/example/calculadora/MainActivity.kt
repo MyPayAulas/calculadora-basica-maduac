ea2115f6cfd841df99fab86e9eef82f0d4f97898
Maria Eduarda Assunção Cavalcante <meac.eng19@uea.edu.br>
2020-12-19 17:52:05 -0400

    feat: adiciona tela inicial

--- Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
+++ Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -1,13 +1,24 @@
 package com.example.calculadora
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.calculadora.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.calculadora)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar!!.hide()
+
+        inicio.setOnClickListener {
+            val intent = Intent(this, Calculadora::class.java)
+            startActivity(intent) //Trocar de Activity
+            finish() //finalizar MainActivity
+        }
+
 
     }
 }
