8033b64e026857f6de6424a07b9924587d5c7cc9
Maria Eduarda Assunção Cavalcante <meac.eng19@uea.edu.br>
2020-12-19 16:42:32 -0400

    feat: criação do layout

---
+++ Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.calculadora
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.calculadora.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.calculadora)
+
+    }
+}
